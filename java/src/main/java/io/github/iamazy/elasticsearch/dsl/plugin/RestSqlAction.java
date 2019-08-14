828501bb203b43919fbb7912f04ec139b07a753d
iamazy <1448588084@qq.com>
2019-08-14 22:58:01 +0800

    Update RestSqlAction.java

--- java/src/main/java/io/github/iamazy/elasticsearch/dsl/plugin/RestSqlAction.java
+++ java/src/main/java/io/github/iamazy/elasticsearch/dsl/plugin/RestSqlAction.java
@@ -2,7 +2,6 @@ package io.github.iamazy.elasticsearch.dsl.plugin;
 
 import com.carrotsearch.hppc.cursors.ObjectObjectCursor;
 import io.github.iamazy.elasticsearch.dsl.sql.ElasticSql2DslParser;
-import io.github.iamazy.elasticsearch.dsl.sql.exception.ElasticSql2DslException;
 import io.github.iamazy.elasticsearch.dsl.sql.model.ElasticSqlParseResult;
 import org.apache.commons.lang3.StringUtils;
 import org.elasticsearch.client.node.NodeClient;
@@ -13,15 +12,12 @@ import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.common.xcontent.XContentParser;
 import org.elasticsearch.common.xcontent.XContentType;
-import org.elasticsearch.index.reindex.BulkByScrollTask;
 import org.elasticsearch.index.reindex.ReindexAction;
 import org.elasticsearch.rest.*;
 import org.elasticsearch.tasks.LoggingTaskListener;
 import org.elasticsearch.tasks.Task;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 
 /**
@@ -62,19 +58,32 @@ public class RestSqlAction extends BaseRestHandler {
             if (restRequest.path().endsWith("/_explain")) {
                 return channel -> channel.sendResponse(new BytesRestResponse(RestStatus.OK, builder.value(parseResult.toRequest().source())));
             } else {
-                if (parseResult.toFieldMapping() != null) {
-                    return channel -> channel.sendResponse(new BytesRestResponse(RestStatus.OK, builder.value(nodeClient.admin().indices().getFieldMappings(parseResult.toFieldMapping()).actionGet())));
-                } else if (parseResult.toMapping() != null) {
-                    ImmutableOpenMap<String, MappingMetaData> objectObjectCursors = nodeClient.admin().indices().getMappings(parseResult.toMapping()).actionGet().mappings().get(parseResult.getMappingsRequest().indices()[0]);
-                    for (ObjectObjectCursor<String, MappingMetaData> objectObjectCursor : objectObjectCursors) {
-                        return channel -> channel.sendResponse(new BytesRestResponse(RestStatus.OK, builder.value(objectObjectCursor.value.getSourceAsMap())));
+                switch (parseResult.getSqlOperation()){
+                    case DESC:{
+                        if (parseResult.getFieldMappingsRequest() != null) {
+                            return channel -> channel.sendResponse(new BytesRestResponse(RestStatus.OK, builder.value(nodeClient.admin().indices().getFieldMappings(parseResult.getFieldMappingsRequest()).actionGet())));
+                        } else if (parseResult.getMappingsRequest() != null) {
+                            ImmutableOpenMap<String, MappingMetaData> objectObjectCursors = nodeClient.admin().indices().getMappings(parseResult.getMappingsRequest()).actionGet().mappings().get(parseResult.getMappingsRequest().indices()[0]);
+                            for (ObjectObjectCursor<String, MappingMetaData> objectObjectCursor : objectObjectCursors) {
+                                return channel -> channel.sendResponse(new BytesRestResponse(RestStatus.OK, builder.value(objectObjectCursor.value.getSourceAsMap())));
+                            }
+                            return channel -> channel.sendResponse(new BytesRestResponse(RestStatus.BAD_REQUEST, XContentType.JSON.mediaType(), "{\"error\":\"sql parse failed!!!\"}"));
+                        }
                     }
-                    return channel -> channel.sendResponse(new BytesRestResponse(RestStatus.BAD_REQUEST, XContentType.JSON.mediaType(), "{\"error\":\"sql parse failed!!!\"}"));
-                } else if (parseResult.getReindexRequest() != null) {
-                    return sendTask(nodeClient.getLocalNodeId(), nodeClient.executeLocally(ReindexAction.INSTANCE, parseResult.getReindexRequest(), LoggingTaskListener.instance()));
-                } else {
-                    return channel -> channel.sendResponse(new BytesRestResponse(RestStatus.OK, builder.value(nodeClient.search(parseResult.toRequest()).actionGet())));
+                    case REINDEX:{
+                        return sendTask(nodeClient.getLocalNodeId(), nodeClient.executeLocally(ReindexAction.INSTANCE, parseResult.getReindexRequest(), LoggingTaskListener.instance()));
+                    }
+                    case DELETE:
+                    case INSERT:
+                    case UPDATE:{
+                        return channel -> channel.sendResponse(new BytesRestResponse(RestStatus.BAD_REQUEST, XContentType.JSON.mediaType(), "{\"error\":\"not support yet!!!\"}"));
+                    }
+                    default:
+                    case SELECT:{
+                        return channel -> channel.sendResponse(new BytesRestResponse(RestStatus.OK, builder.value(nodeClient.search(parseResult.toRequest()).actionGet())));
+                    }
                 }
+
             }
         } catch (Exception e) {
             return channel -> channel.sendResponse(new BytesRestResponse(RestStatus.INTERNAL_SERVER_ERROR, XContentType.JSON.mediaType(), "{\"error\":\"" + e.getMessage() + "\"}"));
