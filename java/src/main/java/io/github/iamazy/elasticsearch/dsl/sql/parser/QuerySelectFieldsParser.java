54f50302eea24dd8561ef2a4ec93289da9183932
jiangnan <1448588084@qq.com>
2019-08-15 19:37:48 +0800

    Update QuerySelectFieldsParser.java

--- java/src/main/java/io/github/iamazy/elasticsearch/dsl/sql/parser/QuerySelectFieldsParser.java
+++ java/src/main/java/io/github/iamazy/elasticsearch/dsl/sql/parser/QuerySelectFieldsParser.java
@@ -2,7 +2,6 @@ package io.github.iamazy.elasticsearch.dsl.sql.parser;
 
 import io.github.iamazy.elasticsearch.dsl.antlr4.ElasticsearchParser;
 import io.github.iamazy.elasticsearch.dsl.sql.model.ElasticDslContext;
-import io.github.iamazy.elasticsearch.dsl.sql.enums.SqlOperation;
 import org.apache.commons.collections4.CollectionUtils;
 import org.elasticsearch.search.collapse.CollapseBuilder;
 
@@ -19,7 +18,6 @@ public class QuerySelectFieldsParser implements QueryParser {
     @Override
     public void parse(ElasticDslContext dslContext) {
         if (dslContext.getSqlContext().selectOperation() != null) {
-            dslContext.getParseResult().setSqlOperation(SqlOperation.SELECT);
             if (dslContext.getSqlContext().selectOperation().groupByClause() == null) {
                 ElasticsearchParser.SelectOperationContext selectOperationContext = dslContext.getSqlContext().selectOperation();
                 ElasticsearchParser.FieldListContext fieldListContext = selectOperationContext.fieldList();
