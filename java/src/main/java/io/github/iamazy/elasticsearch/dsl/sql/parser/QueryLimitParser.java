c36aec477bbdaa700d23554af102b2692abaaf46
jiangnan <1448588084@qq.com>
2019-08-15 19:24:01 +0800

    Update QueryLimitParser.java

--- java/src/main/java/io/github/iamazy/elasticsearch/dsl/sql/parser/QueryLimitParser.java
+++ java/src/main/java/io/github/iamazy/elasticsearch/dsl/sql/parser/QueryLimitParser.java
@@ -15,13 +15,20 @@ public class QueryLimitParser implements QueryParser{
     public void parse(ElasticDslContext dslContext) {
         if(dslContext.getSqlContext().selectOperation()!=null){
             ElasticsearchParser.LimitClauseContext limitClauseContext=dslContext.getSqlContext().selectOperation().limitClause();
-            if(limitClauseContext==null){
-                return;
-            }
-            if(limitClauseContext.offset!=null){
-                dslContext.getParseResult().setFrom(Integer.parseInt(limitClauseContext.offset.getText()));
-            }
-            dslContext.getParseResult().setSize(Integer.parseInt(limitClauseContext.size.getText()));
+            parseLimit(limitClauseContext,dslContext);
+        }else if(dslContext.getSqlContext().deleteOperation()!=null){
+            ElasticsearchParser.LimitClauseContext limitClauseContext=dslContext.getSqlContext().deleteOperation().limitClause();
+            parseLimit(limitClauseContext,dslContext);
         }
+    }
+
+    private void parseLimit(ElasticsearchParser.LimitClauseContext limitClauseContext,ElasticDslContext dslContext){
+        if(limitClauseContext==null){
+            return;
+        }
+        if(limitClauseContext.offset!=null){
+            dslContext.getParseResult().setFrom(Integer.parseInt(limitClauseContext.offset.getText()));
+        }
+        dslContext.getParseResult().setSize(Integer.parseInt(limitClauseContext.size.getText()));
     }
 }
