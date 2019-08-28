56eddef708551c79cd3eec49bf3483f65e548072
jiangnan <1448588084@qq.com>
2019-08-28 16:16:33 +0800

    Update BoolExpressionParser.java

--- java/src/main/java/io/github/iamazy/elasticsearch/dsl/sql/parser/BoolExpressionParser.java
+++ java/src/main/java/io/github/iamazy/elasticsearch/dsl/sql/parser/BoolExpressionParser.java
@@ -18,7 +18,6 @@ import org.elasticsearch.index.query.QueryBuilders;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * @author iamazy
