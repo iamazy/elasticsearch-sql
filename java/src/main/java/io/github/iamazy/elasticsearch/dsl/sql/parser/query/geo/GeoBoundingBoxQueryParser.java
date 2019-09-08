55a728db042a619125074db97b95d49d7e3ae6cf
jiangnan <1448588084@qq.com>
2019-09-08 15:30:53 +0800

    Update GeoBoundingBoxQueryParser.java

--- java/src/main/java/io/github/iamazy/elasticsearch/dsl/sql/parser/query/geo/GeoBoundingBoxQueryParser.java
+++ java/src/main/java/io/github/iamazy/elasticsearch/dsl/sql/parser/query/geo/GeoBoundingBoxQueryParser.java
@@ -3,7 +3,9 @@ package io.github.iamazy.elasticsearch.dsl.sql.parser.query.geo;
 import io.github.iamazy.elasticsearch.dsl.antlr4.ElasticsearchParser;
 import io.github.iamazy.elasticsearch.dsl.sql.model.AtomicQuery;
 import io.github.iamazy.elasticsearch.dsl.sql.parser.ExpressionQueryParser;
+import io.github.iamazy.elasticsearch.dsl.utils.GeoUtils;
 import io.github.iamazy.elasticsearch.dsl.utils.StringManager;
+import org.elasticsearch.common.geo.GeoPoint;
 import org.elasticsearch.index.query.GeoBoundingBoxQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 
@@ -18,14 +20,9 @@ public class GeoBoundingBoxQueryParser implements ExpressionQueryParser<Elastics
     @Override
     public AtomicQuery parse(ElasticsearchParser.GeoBoundingBoxClauseContext expression) {
         GeoBoundingBoxQueryBuilder geoBoundingBoxQueryBuilder = QueryBuilders.geoBoundingBoxQuery(expression.field.getText());
-        if (expression.leftTop.getText().contains(",")&&expression.rightBottom.getText().contains(",")) {
-            String[] leftTop = StringManager.removeStringSymbol(expression.leftTop.getText()).split(",");
-            String[] rightBottom = StringManager.removeStringSymbol(expression.rightBottom.getText()).split(",");
-            geoBoundingBoxQueryBuilder.setCorners(Double.parseDouble(leftTop[0]), Double.parseDouble(leftTop[1]),Double.parseDouble(rightBottom[0]),Double.parseDouble(rightBottom[1]));
-        } else {
-            geoBoundingBoxQueryBuilder.setCorners(StringManager.removeStringSymbol(expression.leftTop.getText()),StringManager.removeStringSymbol(expression.rightBottom.getText()));
-        }
-        return new AtomicQuery(geoBoundingBoxQueryBuilder);
+        GeoPoint leftTop = GeoUtils.parseGeoPoint(expression.leftTop);
+        GeoPoint rightBottom = GeoUtils.parseGeoPoint(expression.rightBottom);
+        return new AtomicQuery(geoBoundingBoxQueryBuilder.setCorners(leftTop, rightBottom));
     }
 
     @Override
