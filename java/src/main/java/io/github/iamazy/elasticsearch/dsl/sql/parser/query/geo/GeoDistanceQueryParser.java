fd04f247439a7fd2d78bc32cdd5b433c1f1f2fe7
jiangnan <1448588084@qq.com>
2019-09-08 15:30:42 +0800

    Update GeoDistanceQueryParser.java

--- java/src/main/java/io/github/iamazy/elasticsearch/dsl/sql/parser/query/geo/GeoDistanceQueryParser.java
+++ java/src/main/java/io/github/iamazy/elasticsearch/dsl/sql/parser/query/geo/GeoDistanceQueryParser.java
@@ -4,6 +4,7 @@ package io.github.iamazy.elasticsearch.dsl.sql.parser.query.geo;
 import io.github.iamazy.elasticsearch.dsl.antlr4.ElasticsearchParser;
 import io.github.iamazy.elasticsearch.dsl.sql.model.AtomicQuery;
 import io.github.iamazy.elasticsearch.dsl.sql.parser.ExpressionQueryParser;
+import io.github.iamazy.elasticsearch.dsl.utils.GeoUtils;
 import io.github.iamazy.elasticsearch.dsl.utils.StringManager;
 import org.elasticsearch.index.query.GeoDistanceQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
@@ -18,14 +19,9 @@ public class GeoDistanceQueryParser implements ExpressionQueryParser<Elasticsear
 
     @Override
     public AtomicQuery parse(ElasticsearchParser.GeoDistanceClauseContext expression) {
-        GeoDistanceQueryBuilder geoDistanceQueryBuilder= QueryBuilders.geoDistanceQuery(StringManager.removeStringSymbol(expression.ID().getText()));
-        if(expression.coordinate.getText().contains(",")){
-            String[] latLon=StringManager.removeStringSymbol(expression.coordinate.getText()).split(",");
-            geoDistanceQueryBuilder.point(Double.parseDouble(latLon[0]),Double.parseDouble(latLon[1]));
-        }else{
-            geoDistanceQueryBuilder.geohash(StringManager.removeStringSymbol(expression.coordinate.getText()));
-        }
-        geoDistanceQueryBuilder.distance(StringManager.removeStringSymbol(expression.distance.getText()));
+        GeoDistanceQueryBuilder geoDistanceQueryBuilder= QueryBuilders.geoDistanceQuery(StringManager.removeStringSymbol(expression.ID().getText()))
+                .point(GeoUtils.parseGeoPoint(expression.geoPointClause()))
+                .distance(StringManager.removeStringSymbol(expression.distance.getText()));
         return new AtomicQuery(geoDistanceQueryBuilder);
     }
 
