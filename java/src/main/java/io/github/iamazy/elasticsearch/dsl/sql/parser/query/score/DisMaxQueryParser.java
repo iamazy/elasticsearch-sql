c1348b7f607bd3e6fb719beb222ac3b6cd284b6a
jiangnan <1448588084@qq.com>
2019-09-10 13:23:54 +0800

    Create DisMaxQueryParser.java

---
+++ java/src/main/java/io/github/iamazy/elasticsearch/dsl/sql/parser/query/score/DisMaxQueryParser.java
@@ -0,0 +1,33 @@
+package io.github.iamazy.elasticsearch.dsl.sql.parser.query.score;
+
+import io.github.iamazy.elasticsearch.dsl.antlr4.ElasticsearchParser;
+import io.github.iamazy.elasticsearch.dsl.sql.model.ElasticDslContext;
+import io.github.iamazy.elasticsearch.dsl.sql.parser.BoolExpressionParser;
+import io.github.iamazy.elasticsearch.dsl.sql.parser.QueryParser;
+import org.elasticsearch.index.query.DisMaxQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+
+/**
+ * @author iamazy
+ * @date 2019/9/10
+ * @descrition
+ **/
+public class DisMaxQueryParser implements QueryParser {
+
+
+    @Override
+    public void parse(ElasticDslContext dslContext) {
+        if(dslContext.getSqlContext().selectOperation()!=null&&dslContext.getSqlContext().selectOperation().disMaxClause()!=null){
+            ElasticsearchParser.DisMaxClauseContext disMaxClauseContext = dslContext.getSqlContext().selectOperation().disMaxClause();
+            BoolExpressionParser boolExpressionParser=new BoolExpressionParser();
+            DisMaxQueryBuilder queryBuilder=QueryBuilders.disMaxQuery();
+            for(ElasticsearchParser.ExpressionContext expressionContext:disMaxClauseContext.expression()){
+                queryBuilder.add(boolExpressionParser.parseBoolQueryExpr(expressionContext));
+            }
+            if(disMaxClauseContext.tieBreaker!=null){
+                queryBuilder.tieBreaker(Float.parseFloat(disMaxClauseContext.tieBreaker.getText()));
+            }
+            dslContext.getParseResult().setWhereCondition(queryBuilder);
+        }
+    }
+}
