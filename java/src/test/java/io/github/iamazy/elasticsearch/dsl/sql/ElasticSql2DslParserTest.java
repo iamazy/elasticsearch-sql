4acc40ac72634c13fb3b15da35eb9fe49cffd741
jiangnan <1448588084@qq.com>
2019-09-11 16:16:33 +0800

    add track_total_hits support

--- java/src/test/java/io/github/iamazy/elasticsearch/dsl/sql/ElasticSql2DslParserTest.java
+++ java/src/test/java/io/github/iamazy/elasticsearch/dsl/sql/ElasticSql2DslParserTest.java
@@ -245,4 +245,14 @@ public class ElasticSql2DslParserTest {
         System.out.println(parseResult.toPrettyDsl(parseResult.toRequest()));
         System.out.println(System.currentTimeMillis()-now);
     }
+
+    @Test
+    public void trackTotalHits(){
+        long now=System.currentTimeMillis();
+        String sql="select * from student track total";
+        ElasticSql2DslParser parser=new ElasticSql2DslParser();
+        ElasticSqlParseResult parseResult = parser.parse(sql);
+        System.out.println(parseResult.toPrettyDsl(parseResult.toRequest()));
+        System.out.println(System.currentTimeMillis()-now);
+    }
 }
