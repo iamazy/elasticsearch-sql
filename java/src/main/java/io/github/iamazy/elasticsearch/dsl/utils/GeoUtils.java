32368b454c6ca6883b58797dd94c2a6e7b32aa48
jiangnan <1448588084@qq.com>
2019-09-23 18:58:33 +0800

    Update GeoUtils.java

--- java/src/main/java/io/github/iamazy/elasticsearch/dsl/utils/GeoUtils.java
+++ java/src/main/java/io/github/iamazy/elasticsearch/dsl/utils/GeoUtils.java
@@ -2,6 +2,7 @@ package io.github.iamazy.elasticsearch.dsl.utils;
 
 import io.github.iamazy.elasticsearch.dsl.antlr4.ElasticsearchParser;
 import org.elasticsearch.common.geo.GeoPoint;
+import org.elasticsearch.common.geo.ShapeRelation;
 
 /**
  * @author iamazy
@@ -12,5 +13,23 @@ public class GeoUtils {
 
     public static GeoPoint parseGeoPoint(ElasticsearchParser.PointContext pointContext) {
         return new GeoPoint(Double.parseDouble(pointContext.lat.getText()), Double.parseDouble(pointContext.lon.getText()));
+    }
+
+    public static ShapeRelation parseGeoRelation(int relation){
+        switch (relation){
+            case ElasticsearchParser.WITHIN:{
+                return ShapeRelation.WITHIN;
+            }
+            case ElasticsearchParser.DISJOINT:{
+                return ShapeRelation.DISJOINT;
+            }
+            case ElasticsearchParser.CONTAINS:{
+                return ShapeRelation.CONTAINS;
+            }
+            default:
+            case ElasticsearchParser.INTERSECTS:{
+                return ShapeRelation.INTERSECTS;
+            }
+        }
     }
 }
