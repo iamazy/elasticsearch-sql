4c9a0dbedd1596278e8166cae286ec021ebda289
jiangnan <1448588084@qq.com>
2019-10-29 17:02:06 +0800

    add open(close) inteval support -> (1,2]

--- java/src/main/java/io/github/iamazy/elasticsearch/dsl/sql/parser/query/exact/BetweenAndQueryParser.java
+++ java/src/main/java/io/github/iamazy/elasticsearch/dsl/sql/parser/query/exact/BetweenAndQueryParser.java
@@ -5,6 +5,7 @@ import io.github.iamazy.elasticsearch.dsl.sql.enums.SqlConditionOperator;
 import io.github.iamazy.elasticsearch.dsl.sql.model.AtomicQuery;
 import io.github.iamazy.elasticsearch.dsl.utils.StringManager;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.RangeQueryBuilder;
 
 /**
  * @author iamazy
@@ -15,6 +16,24 @@ public class BetweenAndQueryParser extends AbstractExactQueryParser {
 
 
     public AtomicQuery parse(ElasticsearchParser.BetweenAndContext betweenAndContext) {
+        if(betweenAndContext.rangeClause()!=null){
+            ElasticsearchParser.RangeClauseContext rangeClauseContext=betweenAndContext.rangeClause();
+            String field=rangeClauseContext.field.getText();
+            RangeQueryBuilder queryBuilder=QueryBuilders.rangeQuery(field);
+            String leftValue = StringManager.removeStringSymbol(rangeClauseContext.left.getText());
+            String rightValue = StringManager.removeStringSymbol(rangeClauseContext.right.getText());
+            if(rangeClauseContext.LPAREN()!=null){
+                queryBuilder.gt(leftValue);
+            }else{
+                queryBuilder.gte(leftValue);
+            }
+            if(rangeClauseContext.RPAREN()!=null){
+                queryBuilder.lt(rightValue);
+            }else{
+                queryBuilder.lte(rightValue);
+            }
+            return new AtomicQuery(queryBuilder);
+        }
         return parseCondition(betweenAndContext, SqlConditionOperator.BetweenAnd,
                 new Object[]{betweenAndContext.left.getText(), betweenAndContext.right.getText()},
                 (field, operator, params) -> QueryBuilders.rangeQuery(field).gte(StringManager.removeStringSymbol(params[0].toString())).lte(StringManager.removeStringSymbol(params[1].toString())));
