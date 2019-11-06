b62c10d7f051ac5e9260436a9d44c9a032f04880
jiangnan <1448588084@qq.com>
2019-11-06 15:51:29 +0800

    Update TermsAggregationParser.java

--- java/src/main/java/io/github/iamazy/elasticsearch/dsl/sql/parser/aggs/bucket/TermsAggregationParser.java
+++ java/src/main/java/io/github/iamazy/elasticsearch/dsl/sql/parser/aggs/bucket/TermsAggregationParser.java
@@ -9,7 +9,6 @@ import org.elasticsearch.search.aggregations.AggregationBuilders;
 import org.elasticsearch.search.aggregations.BucketOrder;
 
 
-
 /**
  * @author iamazy
  * @date 2019-08-03
@@ -25,10 +24,13 @@ public class TermsAggregationParser implements AggregationParser {
         if (StringUtils.equalsIgnoreCase(aggregateItemClauseContext.ID().getText(), AGG_TERMS_METHOD)) {
             try {
                 String field = aggregateItemClauseContext.collection().identity(0).getText();
-                int size = Integer.parseInt(aggregateItemClauseContext.collection().identity(1).getText());
-                aggregateQuery.setAggregationBuilder(AggregationBuilders.terms(field).size(size)
+                int size = 10;
+                if (aggregateItemClauseContext.collection().identity().size() > 1) {
+                    size = Integer.parseInt(aggregateItemClauseContext.collection().identity(1).getText());
+                }
+                aggregateQuery.setAggregationBuilder(AggregationBuilders.terms(field + "_terms").field(field).size(size)
                         .minDocCount(1).shardMinDocCount(1).shardSize(size << 1).order(BucketOrder.count(false)));
-            }catch (IllegalArgumentException e){
+            } catch (IllegalArgumentException e) {
                 throw new ElasticSql2DslException(e);
             }
         }
