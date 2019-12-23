2c2cc43c834f869fd66829d4bd81ee20cf52a751
jiangnan <1448588084@qq.com>
2019-12-23 09:12:54 +0800

    Update FlatMapTest.java

--- java/src/test/java/io/github/iamazy/elasticsearch/dsl/sql/FlatMapTest.java
+++ java/src/test/java/io/github/iamazy/elasticsearch/dsl/sql/FlatMapTest.java
@@ -3,7 +3,9 @@ package io.github.iamazy.elasticsearch.dsl.sql;
 import io.github.iamazy.elasticsearch.dsl.utils.FlatMapUtils;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -16,14 +18,15 @@ public class FlatMapTest {
     @Test
     public void flatTest(){
         Map<String,Object> map=new HashMap<>(0);
-        FlatMapUtils.flatPut("deviceInfo.deviceLocation.zhCountry","中国",map);
-        FlatMapUtils.flatPut("deviceInfo.deviceCategory","M_RSS",map);
-        FlatMapUtils.flatPut("deviceInfo.deviceLocation.zhProvince","台湾",map);
-        FlatMapUtils.flatPut("deviceInfo.deviceLocation.zhCity","台中市",map);
-        System.out.println(map);
-        Object o = FlatMapUtils.flatGet("deviceInfo.deviceLocation.zhCity", map);
-        System.out.println(o);
-        Object o1 = FlatMapUtils.flatGet("deviceInfo.deviceCategory", map);
-        System.out.println(o1);
+        List<Integer> list=new ArrayList<>(0);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        FlatMapUtils.flatPut("parent.list1","list",map);
+        FlatMapUtils.flatPut("parent.list3.list2",list,map);
+        Map<String, Object> flat = FlatMapUtils.flat(map, null);
+        System.out.println(flat);
     }
 }
